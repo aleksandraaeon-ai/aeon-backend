85e1f5938311b5f974f0aeff81de004fa9dd860e
Aleksandraaeon <aleksandra.stanojevic@hotmail.com>
2025-04-06 21:15:03 +0200

    Prebačen serviceAccount na FIREBASE_KEY iz env

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
